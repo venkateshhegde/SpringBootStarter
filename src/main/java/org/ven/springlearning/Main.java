30a0acc268c1f7fd7d122cb8f4677c0b1c0639d4
ven <venkateshhegde@yahoo.com>
2019-10-21 08:35:32 -0400

    Added new style of HTTP /first/last

--- src/main/java/org/ven/springlearning/Main.java
+++ src/main/java/org/ven/springlearning/Main.java
@@ -1,54 +1,51 @@
 package org.ven.springlearning;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 //Spring Boot App
 @SpringBootApplication ()
 //RESTFul services
 @RestController
+@RequestMapping(value="hello")
+
 public class Main implements CommandLineRunner {
 
 
-    //HTTPS Support
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
+//    //HTTPS Support
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//
+//        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+//        return tomcat;
+//    }
 
-        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-        return tomcat;
-    }
-
-    //HTTPS Support
-    private Connector initiateHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8081);
-
-        return connector;
-    }
+//    //HTTPS Support
+//    private Connector initiateHttpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8081);
+//
+//        return connector;
+//    }
 
 //Dependency Injection
     @Autowired
@@ -68,14 +65,21 @@ public class Main implements CommandLineRunner {
         fs.showBarAccess();fs.showBarAccess();fs.showBarAccess();
     }
 
-    @RequestMapping("/")
-    public String  defaultPage() {
+    @RequestMapping(value="/{firstName}/{lastName}",method = RequestMethod.GET)
+    public String hello( @PathVariable("firstName") String firstName,
+                         @PathVariable("lastName") String lastName) {
 
-        return "<h1>HELLO GREAT WORLD!</h1>" +
-                //Live reload support on web pages
-                "<a href src=\"https://github.com/guard/guard-livereload\"></a>" +
-                "";
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
     }
+//
+//    @RequestMapping("/")
+//    public String  defaultPage() {
+//
+//        return "<h1>HELLO GREAT WORLD!</h1>" +
+//                //Live reload support on web pages
+//                "<a href src=\"https://github.com/guard/guard-livereload\"></a>" +
+//                "";
+//    }
 }
 
 
