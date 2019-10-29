7f0c3e6600026a1c80155f3ccb6d70765cc61426
ven <venkateshhegde@yahoo.com>
2019-10-29 17:48:54 -0400

    Added Token

--- src/main/java/org/ven/springlearning/FooImpl2.java
+++ src/main/java/org/ven/springlearning/FooImpl2.java
@@ -1,12 +1,25 @@
 package org.ven.springlearning;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-
+@ManagedResource
 @Component
 public class FooImpl2 implements IFoo {
+
+    @ManagedAttribute(description = "Bean Attr")
+    public int getValueToBeUsed() {
+        return valueToBeUsed;
+    }
+    @ManagedAttribute(description = "Bean Attr set")
+    public void setValueToBeUsed(int valueToBeUsed) {
+        this.valueToBeUsed = valueToBeUsed;
+    }
+
+
 
     //Spring properties with default integer
     @Value("${org.ven.testMe: 210}")
@@ -15,6 +28,8 @@ public class FooImpl2 implements IFoo {
     //Spring properties with default Strings
     @Value("${org.ven.testMeStr:Hello World\n\n}")
     private String valueToBeUsedStr;
+
+    ////a29fa0dadf898dec2e37a246e64042b8b49a902e
 
     //Spring properties with default Arrays
     @Value("${org.ven.testMeArr:Hello,World,One}")
